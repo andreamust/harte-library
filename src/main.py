da78babc4cee2b592ee1013d0bd2812c767fb6a4
Andrea Poltronieri <andrea.poltronieri21@gmail.com>
2022-05-24 23:37:02 +0200

    main class sketched

--- src/main.py
+++ src/main.py
@@ -1,3 +1,26 @@
 """
 
 """
+
+
+class Hartify():
+    """
+
+    """
+    def __init__(self, harte_chord: str) -> None:
+        """
+
+        :param harte_chord:
+        """
+        if type(harte_chord) != str or ':' not in harte_chord:
+            raise ValueError('The given chord is not a valid Harte Chord.')
+        self.harte_chord = harte_chord
+
+    def __del__(self):
+        """
+
+        :return:
+        """
+        print('ciao')
+
+Hartify(0)
