10d93250c6db929cab0ff5619e63e0949f587a7a
andreamust <andrea.poltronieri21@gmail.com>
2022-12-09 12:48:32 +0100

    reformat mappings

--- harte/mappings.py
+++ harte/mappings.py
@@ -1,3 +1,10 @@
+"""
+Map of the Harte shorthands available, mapped to their correspondent
+chord tones/grades.
+"""
+
+from collections import OrderedDict
+
 SHORTHAND_DEGREES = {
     "": ["1", "3", "5"],
     "maj": ["1", "3", "5"],
@@ -45,3 +52,41 @@ SHORTHAND_DEGREES = {
     "power": ["1", "5"],
     "pedal": ["1", ]
 }
+
+SHORTHAND_DEGREE_MAP = {
+    'maj': ('3', '5'),
+    'min': ('b3', '5'),
+    'aug': ('3', '#5'),
+    'dim': ('b3', 'b5'),
+    'maj7': ('3', '5', '7'),
+    'min7': ('b3', '5', 'b7'),
+    '7': ('3', '5', 'b7'),
+    'minmaj7': ('b3', '5', '7'),
+    'dim7': ('b3', 'b5', 'bb7'),
+    'hdim7': ('b3', 'b5', 'b7'),
+    'maj6': ('3', '5', '6'),
+    'min6': ('b3', '5', '6'),
+    '9': ('3', '5', 'b7', '9'),
+    'maj9': ('3', '5', '7', '9'),
+    'min9': ('b3', '5', 'b7', '9'),
+    'sus4': ('4', '5'),
+}
+
+DEGREE_SHORTHAND_MAP = OrderedDict({
+    ('3', '5', 'b7', '9'): '9',
+    ('3', '5', '7', '9'): 'maj9',
+    ('b3', '5', 'b7', '9'): 'min9',
+    ('3', '5', 'b7'): '7',
+    ('3', '5', '6'): 'maj6',
+    ('b3', '5', '6'): 'min6',
+    ('3', '5', '7'): 'maj7',
+    ('b3', 'b5', 'bb7'): 'dim7',
+    ('b3', '5', 'b7'): 'min7',
+    ('b3', 'b5', 'b7'): 'hdim7',
+    ('b3', '5', '7'): 'minmaj7',
+    ('3', '5'): 'maj',
+    ('b3', '5'): 'min',
+    ('b3', 'b5'): 'dim',
+    ('3', '#5'): 'aug',
+    ('4', '5'): 'sus4',
+})
