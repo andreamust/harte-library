4e69f3a88eec1559af11a9192961877ce635585c
andreamust <andrea.poltronieri21@gmail.com>
2022-12-09 14:44:01 +0100

    add setup.py

---
+++ harte/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="harte-library",
+    version="0.0.1",
+    author="Andrea Poltronieri",
+    description="Library for parsing Harte chords and converting them to Music21",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    py_modules=["harte-library"],
+    package_dir={'': 'harte'},
+    install_requires=['music21', 'numpy', 'lark'],
+)
