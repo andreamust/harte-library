c89629a441e833242001bf71025e4a75108486ef
Andrea Poltronieri <andrea.poltronieri21@gmail.com>
2022-12-11 22:52:32 +0100

    updated versioning

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="harte-library",
-    version="0.1.5",
+    version="0.2.0",
     author="Andrea Poltronieri",
     description="Library for parsing Harte chords and converting them to Music21",
     long_description=long_description,
